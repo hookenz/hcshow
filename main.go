ea9416d71b354e7abedcf34a722cf6b8908054d1
hookenz <hookenz@gmail.com>
2025-11-19 05:19:43 +1300

    import hc data

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/jsvm"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// Register the JSVM plugin
+	jsvm.MustRegister(app, jsvm.Config{
+		// You can add configurations here if needed,
+		// such as custom Go bindings or an OnInit function.
+	})
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		se.Router.GET("/*", apis.Static(os.DirFS("./pb_public"), false))
+
+		return se.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
